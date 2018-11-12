e43c467b7484721b59b4d6872ac9c680f2a5b043
Marcsllite <marcsllite@gmail.com>
2018-11-12 16:11:07 -0500

    Update scripts/assign6.js

--- scripts/assign6.js
+++ scripts/assign6.js
@@ -1,10 +1,12 @@
 function validateForm() {
-    //event.preventDefault();
+    "use strict";
+    event.preventDefault();
     // getting values from the form
-    var startX = document.getElementById("myForm").elements["startX"].value;
-    var endX = document.getElementById("myForm").elements["endX"].value;
-    var startY = document.getElementById("myForm").elements["startY"].value;
-    var endY = document.getElementById("myForm").elements["endY"].value;
+    var startX = document.getElementById("myForm").elements.startX.value, 
+        endX = document.getElementById("myForm").elements.endX.value, 
+        startY = document.getElementById("myForm").elements.startY.value, 
+        endY = document.getElementById("myForm").elements.endY.value, 
+        table = document.getElementById("myTable");
     
   
     // turning string values of the numers into actual numbers
@@ -15,68 +17,62 @@ function validateForm() {
   
     // Checking if the number from the form was obtained, 
     // if not message send to console and user is alerted
-    if (!Number.isSafeInteger(startX)){
-      if (startX > 0) {
-        console.log("StartX: " + startX + "; StartX is greater than 9007199254740991");
-        alert("The value in 'Start Number (Horizontal)' is greater than the maximum safe number (9,007,199,254,740,991).");
-        return;
-      }
-      else {
-        console.log("StartX: " + startX + "; StartX is less than -9007199254740990");
-        alert("The value in 'Start Number (Horizontal)' is less than the minimum safe number (-9,007,199,254,740,990).");
-        return;
-      }
+    if (!Number.isSafeInteger(startX)) {
+        if (startX > 0) {
+            console.log("StartX: " + startX + "; StartX is greater than 9007199254740991");
+            alert("The value in 'Start Number (Horizontal)' is greater than the maximum safe number (9,007,199,254,740,991).");
+            return;
+        } else {
+            console.log("StartX: " + startX + "; StartX is less than -9007199254740990");
+            alert("The value in 'Start Number (Horizontal)' is less than the minimum safe number (-9,007,199,254,740,990).");
+            return;
+        }
     }
     
-    if (!Number.isSafeInteger(endX)){
-      if (endX > 0) {
-        console.log("EndX: " + endX + "; EndX is greater than 9007199254740991");
-        alert("The value in 'End Number (Horizontal)' is greater than the maximum safe number (9,007,199,254,740,991).");
-        return;
-      }
-      else{
-         console.log("EndX: " + endX + "; EndX is less than -9007199254740990");
-        alert("The value in 'End Number (Horizontal)' is less than the minimum safe number (-9,007,199,254,740,990).");
-        return;
-      }
+    if (!Number.isSafeInteger(endX)) {
+        if (endX > 0) {
+            console.log("EndX: " + endX + "; EndX is greater than 9007199254740991");
+            alert("The value in 'End Number (Horizontal)' is greater than the maximum safe number (9,007,199,254,740,991).");
+            return;
+        } else {
+            console.log("EndX: " + endX + "; EndX is less than -9007199254740990");
+            alert("The value in 'End Number (Horizontal)' is less than the minimum safe number (-9,007,199,254,740,990).");
+            return;
+        }
     }
-    if (!Number.isSafeInteger(startY)){
-      if (startY > 0) {
-        console.log("StartY: " + startY + "; StartY is greater than 9007199254740991");
-        alert("The value in 'Start Number (Vertical)' is greater than the maximum safe number (9,007,199,254,740,991).");
-        return;
-      }
-      else {
-        console.log("StartY: " + startY + "; StartY is less than -9007199254740990");
-        alert("The value in 'Start Number (Vertical)' is less than the minimum safe number (-9,007,199,254,740,990).");
-        return;
-      }
+    
+    if (!Number.isSafeInteger(startY)) {
+        if (startY > 0) {
+            console.log("StartY: " + startY + "; StartY is greater than 9007199254740991");
+            alert("The value in 'Start Number (Vertical)' is greater than the maximum safe number (9,007,199,254,740,991).");
+            return;
+        } else {
+            console.log("StartY: " + startY + "; StartY is less than -9007199254740990");
+            alert("The value in 'Start Number (Vertical)' is less than the minimum safe number (-9,007,199,254,740,990).");
+            return;
+        }
     }
-    if (!Number.isSafeInteger(endY)){
-      if (endY > 0) {
-        console.log("EndY: " + endY + "; EndY is greater than 9007199254740991");
-        alert("The value in 'End Number (Vertical)' is greater than the maximum safe number (9,007,199,254,740,991).");
-        return;
-      }
-      else {
-        console.log("EndY: " + endY + "; EndY is less than -9007199254740990");
-        alert("The value in 'End Number (Vertical)' is less than the minimum safe number (-9,007,199,254,740,990).");
-        return;
-      }
+    
+    if (!Number.isSafeInteger(endY)) {
+        if (endY > 0) {
+            console.log("EndY: " + endY + "; EndY is greater than 9007199254740991");
+            alert("The value in 'End Number (Vertical)' is greater than the maximum safe number (9,007,199,254,740,991).");
+            return;
+        } else {
+            console.log("EndY: " + endY + "; EndY is less than -9007199254740990");
+            alert("The value in 'End Number (Vertical)' is less than the minimum safe number (-9,007,199,254,740,990).");
+            return;
+        }
     }
     
     // Making sure range is not too big. Alerting user if they need to change the range
-    if(Math.abs(startX - endX) > 100000 || Math.abs(startY - endY) > 100000) {
-        alert("One of the Ranges (End Number - Start Number) is greater than 100,000." +
-              "Please use start and end values that are no more than 100,000 apart.");
+    if (Math.abs(startX - endX) > 1000 || Math.abs(startY - endY) > 1000) {
+        alert("One of the Ranges (End Number - Start Number) is greater than 1,000." +
+              "Please use start and end values that are no more than 1,000 apart.");
         return;
-      else {
-        
-      }
     }
     
     // removing old table data if any so there's only one table per submit
-    var table = document.getElementById("myTable");
     while (table.firstChild) {
         table.removeChild(table.firstChild);
     }
@@ -88,59 +84,61 @@ function validateForm() {
 }
 
 function createtable(startX, endX, startY, endY) {
-    var hRange  = []; // creating an array to hold the Horizontal Range
-    var vRange = [];  // creating an array to hold the Vertical Range
-    
+    "use strict";
+    var hRange  = [], // creating an array to hold the Horizontal Range
+        vRange = [],  // creating an array to hold the Vertical Range
+        i;
+  
     // Adding the numbers to the arrays
     // Checking if horizontal range is in descending order and vertical range is in ascending order
     if (startX > endX && startY < endY) { 
         // pushing horizontal values in descending order
-        for (var i = startX; i >= endX; i--) {
-             hRange.push(i);
-         }
+        for (i = startX; i >= endX; i-=1) {
+            hRange.push(i);
+        }
         
         // pushing vertical values in ascending order
-        for (var i = startY; i <= endY; i++) {
-             vRange.push(i);
-         }
+        for (i = startY; i <= endY; i+=1) {
+            vRange.push(i);
+        }
     }
     
     // Checking if vertical range is in descending order and horizontal range is in ascending order
     else if (startX < endX && startY > endY) {  
         // pushing vertical values in descending order
-        for (var i = startY; i >= endY; i--) {
-             vRange.push(i);
+        for (i = startY; i >= endY; i-=1) {
+            vRange.push(i);
         }
         
         // pushing horizontal values in ascending order
-        for (var i = startX; i <= endX; i++) {
-             hRange.push(i);
+        for (i = startX; i <= endX; i+=1) {
+            hRange.push(i);
         }
     }
     
     // Cheching if both ranges are in descending order
-    else if (startX > endX && startY > endY){  
+    else if (startX > endX && startY > endY) {  
         // pushing horizontal values in descending order
-        for (var i = startX; i >= endX; i--) {
-             hRange.push(i);
+        for (i = startX; i >= endX; i-=1) {
+            hRange.push(i);
          }
         
         // pushing vertical values in descending order
-        for (var i = startY; i >= endY; i--) {
-             vRange.push(i);
+        for (i = startY; i >= endY; i-=1) {
+            vRange.push(i);
         }
     }
     
     // Both ranges are in ascending order
     else if (startX < endX && startY < endY) {  
         // pushing horizontal values in ascending order
-        for (var i = startX; i <= endX; i++) {
-             hRange.push(i);
+        for (i = startX; i <= endX; i+=1) {
+            hRange.push(i);
         }
         
         // pushing vertical values in ascending order
-        for (var i = startY; i <= endY; i++) {
-             vRange.push(i);
+        for (i = startY; i <= endY; i+=1) {
+            vRange.push(i);
         }
     }
     
@@ -149,8 +147,8 @@ function createtable(startX, endX, startY, endY) {
         hRange.push(startX);
         
         // pushing vertical values in ascending order
-        for (var i = startY; i <= endY; i++) {
-             vRange.push(i);
+        for (i = startY; i <= endY; i+=1) {
+            vRange.push(i);
         }
     }
     
@@ -159,8 +157,8 @@ function createtable(startX, endX, startY, endY) {
         hRange.push(startX);
         
         // pushing vertical values in descending order
-        for (var i = startY; i >= endY; i--) {
-             vRange.push(i);
+        for (i = startY; i >= endY; i-=1) {
+            vRange.push(i);
         }
     }
     
@@ -169,8 +167,8 @@ function createtable(startX, endX, startY, endY) {
         vRange.push(startY);  
         
         // pushing horizontal values in ascending order
-        for (var i = startX; i <= endX; i++) {
-             hRange.push(i);
+        for (i = startX; i <= endX; i+=1) {
+            hRange.push(i);
         }
     }
     
@@ -179,8 +177,8 @@ function createtable(startX, endX, startY, endY) {
         vRange.push(startY);
         
         // pushing horizontal values in descending order
-        for (var i = startX; i >= endX; i--) {
-             hRange.push(i);
+        for (i = startX; i >= endX; i-=1) {
+            hRange.push(i);
          }
     }
     
@@ -189,7 +187,7 @@ function createtable(startX, endX, startY, endY) {
         hRange.push(startX);
         vRange.push(startY);
     }
-    /*
+    
     // Creating table
     var table = document.getElementById("myTable");  // getting table element from document
     
@@ -197,15 +195,15 @@ function createtable(startX, endX, startY, endY) {
     var r1 = table.insertRow(0);  // creating first row
     var r2, newC = r1.insertCell(-1); // adding blank cell
     
-    for (var i = 0; i < hRange.length; i++) {  // Adding th elements with value of hRange to the table header
+    for (i = 0; i < hRange.length; i+=1) {  // Adding th elements with value of hRange to the table header
         newC = r1.insertCell(-1);
         newC.innerHTML = "<b>" + hRange[i] + "</b>";
     }
 
     // creating remainging rows and adding values to the respective cells
-    for (var v = 1; v <= vRange.length; v++) {
+    for (var v = 1; v <= vRange.length; v+=1) {
         r2 = table.insertRow(v);
-        for (var h = 0; h <= hRange.length; h++) {
+        for (var h = 0; h <= hRange.length; h+=1) {
             if (h == 0) {
                 newC = r2.insertCell(-1);
                 newC.innerHTML = "<b>" + vRange[v-1] + "</b>";
@@ -218,5 +216,4 @@ function createtable(startX, endX, startY, endY) {
         }
     }
     return;
-  */
 }
